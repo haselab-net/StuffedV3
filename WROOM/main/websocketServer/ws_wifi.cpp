36804187da280e159cf5df0b3edb3f6b1997945a
Guo Zile <zlguo1996@gmail.com>
2019-04-13 16:38:17 +0900

    unchange

--- WROOM/main/websocketServer/ws_wifi.cpp
+++ WROOM/main/websocketServer/ws_wifi.cpp
@@ -55,9 +55,6 @@ void initWifi() {
     wifiEventHandler = new SRWifiEventHandler();
     wifi.setWifiEventHandler(wifiEventHandler);
     
-    //if(ssid.size()==0||password.size()==0) becomeAccessPoint();    // become AP
-    //else wifi.connectAP(ssid, password, false);           // become STA
-
-    // test
-    becomeAccessPoint();
+    if(ssid.size()==0||password.size()==0) becomeAccessPoint();    // become AP
+    else wifi.connectAP(ssid, password, false);           // become STA
 }
