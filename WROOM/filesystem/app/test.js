a48dfebed53710553d16e2c292e113963ba842ec
hase <hasevr@gmail.com>
2019-05-15 11:19:35 +0900

    Remove unused js files. (Not all)

--- WROOM/filesystem/app/test.js
+++
@@ -1,6 +0,0 @@
-log('This is ' + module.filename);
-var treeify=require('treeify.js').asTree;
-function show(x) {log(treeify(x, true));}
-//show(this);
-DUKF.gc();
-log('Heapsize: ' + ESP32.getState().heapSize);
