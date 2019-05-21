90d5d142169c47a888f0c220216379c2a3bd62f9
Guo Zile <zlguo1996@gmail.com>
2019-05-21 20:12:41 +0900

    Create sr_callbacks.js

---
+++ WROOM/filesystem/sr_callbacks.js
@@ -0,0 +1,52 @@
+var softrobot = require("sr_softrobot");
+
+var callbacks;
+(function (callbacks) {
+    var CallbacksMap = (function () {
+        function CallbacksMap(keyGenerator) {
+            this.funcs = {};
+            this.keyGenerator = keyGenerator;
+        }
+        CallbacksMap.prototype.add = function (callback, keyOptions) {
+            var key = this.keyGenerator(keyOptions);
+            this.funcs[key] = callback;
+            return key;
+        };
+        CallbacksMap.prototype.callFunc = function (keyOptions, args) {
+            var key = this.keyGenerator(keyOptions);
+            this.funcs[key]();
+        };
+        return CallbacksMap;
+    }());
+    callbacks.CallbacksMap = CallbacksMap;
+    softrobot.message_command.callbacks.touchThresholdArray = [];
+    if (!softrobot.message_command.callbacks.touchQueryer) {
+        softrobot.message_command.callbacks.touchQueryer = setInterval(function () {
+            softrobot.message_command.requireSensorInfo();
+        }, softrobot.message_command.callbacks.touchQueryerInterval);
+    }
+    var touchSensorCallbacks = new CallbacksMap(function (keyOptions) {
+        return keyOptions.sensorId * 2 + keyOptions.threshold * 100 + (keyOptions.exceed ? 1 : 0);
+    });
+    softrobot.message_command.callbacks.callTouchCallback = touchSensorCallbacks.callFunc.bind(touchSensorCallbacks);
+    function bindTouchCallback(touchSensorId, threshold, exceeds, callback) {
+        var option = {
+            sensorId: touchSensorId,
+            threshold: threshold,
+            exceed: exceeds
+        };
+        var key = touchSensorCallbacks.add(callback, option);
+        softrobot.message_command.callbacks.touchThresholdArray.push(option);
+    }
+    callbacks.bindTouchCallback = bindTouchCallback;
+    function onStartTouch(touchSensorId, threshold, callback) {
+        bindTouchCallback(touchSensorId, threshold, false, callback);
+    }
+    callbacks.onStartTouch = onStartTouch;
+    function onEndTouch(touchSensorId, threshold, callback) {
+        bindTouchCallback(touchSensorId, threshold, true, callback);
+    }
+    callbacks.onEndTouch = onEndTouch;
+})(callbacks || (callbacks = {}));
+
+module.exports = callbacks;
