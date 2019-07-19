a38b6bd91d8fac4e29e3bb6d08391c2269cb3100
Guo Zile <zlguo1996@gmail.com>
2019-07-19 16:37:47 +0900

    do not load iot
    IoT prevent ws connection when JS is running

--- WROOM/filesystem/main/runtime.js
+++ WROOM/filesystem/main/runtime.js
@@ -2,7 +2,7 @@ var loops = require("sr_loops");
 var jslib = require("jslib");
 var console = require("console");
 
-ESP32.include("iot.js");
+// ESP32.include("iot.js");
 ESP32.include("sr_softrobot.js");
 ESP32.include("sr_motor.js");
 ESP32.include("sr_callbacks.js");
