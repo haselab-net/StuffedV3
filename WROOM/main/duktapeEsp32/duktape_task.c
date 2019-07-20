70618a12cffa6a74e4408a0ecb0c64362fa4b203
hase <hasevr@gmail.com>
2019-07-21 00:04:05 +0900

    JS stack size was too small.

--- WROOM/main/duktapeEsp32/duktape_task.c
+++ WROOM/main/duktapeEsp32/duktape_task.c
@@ -179,7 +179,7 @@ void duktape_start() {
 	
 	//	create tasks for threads
 	for(int i=0; i<NJSTHREADS; ++i){
-		xTaskCreate(dukEventHandleTask, taskNames[i], 1024*4, &jsThreads[i], tskIDLE_PRIORITY + 1, &jsThreads[i].task);
+		xTaskCreate(dukEventHandleTask, taskNames[i], 1024*8, &jsThreads[i], tskIDLE_PRIORITY + 1, &jsThreads[i].task);
 	}
 	char* cmd = "ESP32.include('/main/runtime.js');";
 	event_newCommandLineEvent(cmd, strlen(cmd), 0);
@@ -223,7 +223,7 @@ void duktape_end(){
 		for(int i=0; i<NJSTHREADS; ++i){
 			if (jsThreads[i].task){
 				remain = true;
-				const char* cmd = ";";
+				char* cmd = ";";
 				event_newCommandLineEvent(cmd, strlen(cmd), 0);
 				vTaskDelay(1);
 			} 
