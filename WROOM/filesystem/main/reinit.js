e7b1fd4e0217dc9b837a5f4e599bb179da67ce69
Guo Zile <zlguo1996@gmail.com>
2019-07-22 16:18:40 +0900

    Update reinit.js

--- WROOM/filesystem/main/reinit.js
+++ WROOM/filesystem/main/reinit.js
@@ -1,8 +1,21 @@
 // reinit global variables
-// init every time restart duktape task
+// called every time restart duktape task
 
-loops.doForever = function () {};
+// NOTE please clear the event queue before call this file
+
+////////////////////////////////////// reset data set by user //////////////////////////
+// reset forever to default function
+loops.doForever = function () {
+    console.log("default forever");
+    jslib.blockPause(1000);
+};
+
+// clear registered touch sensor callbacks
+softrobot.message_command.callbacks.touchThresholdArray = [];
+touchSensorCallbacks.funcs = {};
 
 // softrobot.movement.sendKeyframeQueue.clear();
 
-iot.stopWaitingMQTTEvent();
+// iot.stopWaitingMQTTEvent();
+
+////////////////////////////////////// reset hardware //////////////////////////
