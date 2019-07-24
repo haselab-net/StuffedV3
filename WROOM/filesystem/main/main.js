ef7f588c3c54992d98ce9349862434b0c696a904
Guo Zile <zlguo1996@gmail.com>
2019-07-24 18:41:09 +0900

    test for js restart

--- WROOM/filesystem/main/main.js
+++ WROOM/filesystem/main/main.js
@@ -1,28 +1,27 @@
-// loops.pause(10000);
-// iot.triggerIFTTTEvent("Mf0JcCX_5FLZiz6Svtpjn", "esp_event", "", "", "")
 console.log("defaule main.js");
 
-// while (1) {
-//     softrobot.message_command.requireSensorInfo();
-//     loops.pause
-// }
+var movement_1 = motor.movementDecoder("4 1 6005\n3 0\n842 2276\n1158 397\n2011 2765\n1994 1246")
+for (var i = 0; i < 3; i++) {
+    motor.setMovement(motor.MovementOption.play, movement_1)
+}
 
-// var movement_1 = motor.movementDecoder("4 1 6005\n3 0\n842 2276\n1158 397\n2011 2765\n1994 1246")
-// for (var i = 0; i < 3; i++) {
-//     motor.setMovement(motor.MovementOption.play, movement_1)
-// }
-
-// iot.registerMQTTEvent("event", function (value1, value2, value3) {
-//     console.log("event");
+// iot.registerMQTTEvent("hello", function (value1, value2, value3) {
+//     console.log("hello");
+// })
+// iot.registerMQTTEvent("hi", function (value1, value2, value3) {
+//     console.log("hi");
+// })
+// callbacks.onStartTouch(1, 200, function () {
+//     console.log("touch callback called")
 //     iot.triggerIFTTTEvent(
 //     "Mf0JcCX_5FLZiz6Svtpjn",
-//     "esp_event",
-//     value1,
-//     value2,
-//     value3
-//     );
+//     "sensor_touched",
+//     "",
+//     "",
+//     ""
+//     )
 // })
-// iot.startWaitingMQTTEvent("131.112.182.91", 5000)
+// iot.startWaitingMQTTEvent("192.168.91.146", 5000)
 
 // loops.pause(30000)
 // loops.forever(function () {
