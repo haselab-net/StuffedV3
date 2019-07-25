88ee947f472550b8b93725f9d3006c4a98c74c63
Guo Zile <zlguo1996@gmail.com>
2019-07-25 13:34:41 +0900

    Update runtime.js

--- WROOM/filesystem/main/runtime.js
+++ WROOM/filesystem/main/runtime.js
@@ -2,10 +2,10 @@ var jslib = require("jslib");
 var loops = require("sr_loops");
 var console = require("console");
 
-// ESP32.include("iot.js");
+ESP32.include("iot.js");
 ESP32.include("sr_softrobot.js");
 ESP32.include("sr_motor.js");
-// ESP32.include("sr_callbacks.js");
+ESP32.include("sr_callbacks.js");
 
 jslib.printHeap("heap size after require: ");
 
