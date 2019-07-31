bbb7a808cd089c72bf68c2ea4844e329bd231f9a
Guo Zile <zlguo1996@gmail.com>
2019-07-31 15:05:43 +0900

    Update main.js

--- WROOM/filesystem/main/main.js
+++ WROOM/filesystem/main/main.js
@@ -1,11 +1,11 @@
 console.log("defaule main.js");
 
-// var movement_1 = motor.movementDecoder("default1\n4 1 6005\n3 0\n842 2276\n1158 397\n2011 2765\n1994 1246")
+var movement_1 = motor.movementDecoder("default1\n4 1 6005\n3 0\n842 2276\n1158 397\n2011 2765\n1994 1246")
 // var movement_2 = motor.movementDecoder("default2\n4 1 6005\n3 1\n842 2276\n1158 397\n2011 2765\n1994 1246")
-// loops.forever(function () {
-//     motor.setMovement(motor.MovementOption.play, movement_1)
-//     loops.pause(1000)
-// })
+loops.forever(function () {
+    motor.setMovement(motor.MovementOption.play, movement_1)
+    loops.pause(1000)
+})
 // callbacks.onStartTouch(1, 200, function () {
 //     console.log("on touch");
 //     motor.setMovement(motor.MovementOption.play, movement_2)
