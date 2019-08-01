023c70d1a1900c1c563a0fe27a9c06072eb41a48
Guo Zile <zlguo1996@gmail.com>
2019-08-01 16:08:48 +0900

    fix play movementDecoder memory leak bug

--- WROOM/filesystem/sr_motor.js
+++ WROOM/filesystem/sr_motor.js
@@ -110,23 +110,29 @@ var motor;
         return motor_1.movementNames.length - 1;
     }
     motor_1.getMovementId = getMovementId;
+    function splitToInt(str, seperator) {
+        var nums_str = str.split(seperator);
+        var nums = [];
+        for (var i = 0; i < nums_str.length; i++) {
+            if (nums_str[i]==='') continue;
+            nums.push(Number(nums_str[i]));
+        }
+        return nums;
+    }
+    motor_1.splitToInt = splitToInt;
+    function partialKeyframeDecoder(str) {
+        var nums = motor_1.splitToInt(str, " ");
+        return {
+            period: nums[0],
+            pose: nums.slice(1)
+        };
+    }
+    motor_1.partialKeyframeDecoder = partialKeyframeDecoder;
+    function interpolate(x1, y1, x2, y2, x) {
+        return (y2 - y1) / (x2 - x1) * (x - x1) + y1;
+    }
+    motor_1.interpolate = interpolate;
     function movementDecoder(movementStr) {
-        function splitToInt(str, seperator) {
-            var nums_str = str.split(seperator);
-            var nums = [];
-            for (var i = 0; i < nums_str.length; i++) {
-                if (nums_str[i]==='') continue;
-                nums.push(Number(nums_str[i]));
-            }
-            return nums;
-        }
-        function partialKeyframeDecoder(str) {
-            var nums = splitToInt(str, " ");
-            return {
-                period: nums[0],
-                pose: nums.slice(1)
-            };
-        }
         if (movementStr === "")
             return {
                 movementId: getMovementId("default_name"),
@@ -138,23 +144,20 @@ var motor;
         };
         var lines = movementStr.split("\n");
         var name = lines.splice(0, 1)[0];
-        var line_1 = splitToInt(lines[0], " ");
-        var line_2 = splitToInt(lines[1], " ");
+        var line_1 = motor_1.splitToInt(lines[0], " ");
+        var line_2 = motor_1.splitToInt(lines[1], " ");
         var keyframes_lines = lines.slice(2, 2 + line_1[0]);
         var keyframes = [];
         var time = 0;
         for (var i = 0; i < keyframes_lines.length; i++) {
-            keyframes.push(partialKeyframeDecoder(keyframes_lines[i]));
+            keyframes.push(motor_1.partialKeyframeDecoder(keyframes_lines[i]));
             time += keyframes[keyframes.length - 1].period;
-        }
-        function interpolate(x1, y1, x2, y2, x) {
-            return (y2 - y1) / (x2 - x1) * (x - x1) + y1;
         }
         var keyframe_first = keyframes[0], keyframe_last = keyframes[keyframes.length - 1];
         if (time < line_1[2]) {
             var lastPose = [];
             for (var i = 0; i < line_1[1]; i++) {
-                lastPose.push(interpolate(time - line_1[2], keyframe_last.pose[i], keyframe_first.period, keyframe_first.pose[i], 0));
+                lastPose.push(motor_1.interpolate(time - line_1[2], keyframe_last.pose[i], keyframe_first.period, keyframe_first.pose[i], 0));
             }
             keyframes.push({
                 period: line_1[2] - time,
