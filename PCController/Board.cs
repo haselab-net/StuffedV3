dbf8bc99ab7096986c05d76385cba62cdeced22c
hase <hasevr@gmail.com>
2019-11-28 19:10:11 +0900

    Fix for CI_GETPARAM (add command and return length)

--- PCController/Board.cs
+++ PCController/Board.cs
@@ -82,6 +82,11 @@ namespace PCController
                     return 1 + 1 + nMotor * 2 * 2;
                 case CommandId.CI_RESET_SENSOR:
                     return 1 + 2;
+                case CommandId.CI_GETPARAM:
+                    return 1;
+                default:
+                    System.Diagnostics.Debug.Assert(false);
+                    break;
             }
             Debug.Assert(false);
             return 0;
@@ -94,6 +99,7 @@ namespace PCController
                 case CommandId.CI_DIRECT: return 1 + nMotor * 2 * 2;
                 case CommandId.CI_INTERPOLATE: return 1 + nMotor * 2 + 2 + 1;
                 case CommandId.CI_CURRENT: return 1 + nMotor * 3 * 2;
+                case CommandId.CI_GETPARAM: return 1 + 1 + nMotor * 2 * 2;
             }
             return 0;
         }
