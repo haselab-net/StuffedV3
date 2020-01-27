8a63452e95b85a367ddb9d2422ef111019da0863
Guo Zile <zlguo1996@gmail.com>
2020-01-27 11:47:35 +0900

    update firmware version to release

--- WROOM/main/WroomEnv.h
+++ WROOM/main/WroomEnv.h
@@ -3,7 +3,7 @@
 
 #define USE_DUKTAPE true        //  use Duktape or WifiMan
 
-#define FIRMWARE_VERSION "2.1.3d"         // version of firmware
+#define FIRMWARE_VERSION "2.1.3r"         // version of firmware
 
 #ifdef __cplusplus
 extern "C"{
