06dd43ed6c4db5f6f97aaccca7bee1768d32fb9c
hase <hasevr@gmail.com>
2020-05-26 17:56:31 +0900

    Forget to add this file.

---
+++ PIC/spiPwmDefs.h
@@ -0,0 +1,10 @@
+#ifndef _EXAMPLE_FILE_NAME_H    /* Guard against multiple inclusion */
+#define _EXAMPLE_FILE_NAME_H
+
+#if 1
+#define SPIPWM128
+#else
+#define SPIPWM64
+#endif
+
+#endif /* _EXAMPLE_FILE_NAME_H */
